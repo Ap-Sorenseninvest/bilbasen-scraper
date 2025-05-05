7efc6e3d598dc6f9e40f60ab85180b0ed75bed1c
Alex Sørensen <alex@Mac-mini-tilhrende-Alex.local>
2025-05-05 18:32:40 +0200

    Tilføjet headless=True og rettet Supabase URL

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from bs4 import BeautifulSoup
 import os
 import requests
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_API_KEY = os.getenv("SUPABASE_API_KEY")
+SUPABASE_URL = os.getenv("https://mmhzdntjwkkpflglwchy.supabase.co")
+SUPABASE_API_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1taHpkbnRqd2trcGZsZ2x3Y2h5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NTMxNzA4NywiZXhwIjoyMDYwODkzMDg3fQ.KHCCOUENSAPJuxfJTdcT6a-zESt8HumumDzCz08zwHs")
 TABLE_NAME = "bilbasen_cars"
 
 headers = {
@@ -31,7 +31,7 @@ def scrape_bilbasen():
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto("https://www.bilbasen.dk/brugt/bil?includeengroscvr=true&includeleasing=false&sortby=date&sortorder=desc")
-        
+
         page.wait_for_selector("section.srp_results__2UEV_", timeout=10000)
         html = page.content()
         soup = BeautifulSoup(html, "html.parser")
